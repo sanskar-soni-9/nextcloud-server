b1610ccb5618b004cf325209866495bfe0472377
SebastianKrupinski <krupinskis05@gmail.com>
2024-04-26 06:59:46 -0400

    fix: remove getowner override issue #26668
    Signed-off-by: SebastianKrupinski <krupinskis05@gmail.com>

--- apps/dav/lib/CalDAV/Calendar.php
+++ apps/dav/lib/CalDAV/Calendar.php
@@ -226,9 +226,6 @@ class Calendar extends \Sabre\CalDAV\Calendar implements IRestorable, IShareable
 	}
 
 	public function getOwner(): ?string {
-		if (isset($this->calendarInfo['{http://owncloud.org/ns}owner-principal'])) {
-			return $this->calendarInfo['{http://owncloud.org/ns}owner-principal'];
-		}
 		return parent::getOwner();
 	}
 
